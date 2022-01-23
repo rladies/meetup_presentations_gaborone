ef7310ea9752cd0b5dc8c2d0f90bf5d867ac67f0
simisani ndaba <53818579+sndaba@users.noreply.github.com>
2022-01-23 14:55:34 +0200

    Update R Packages for data cleaning.R

--- R Packages for Data Cleaning/R Packages for data cleaning.R
+++ R Packages for Data Cleaning/R Packages for data cleaning.R
@@ -1,1 +1,22 @@
+#packages and code to be used in the R packages for data cleaning workshop_25/01/2022
 
+install.packages("janitor")
+library(janitor)
+install.packages("naniar")
+library(naniar)
+install.packages("data.table")
+library(data.table)
+install.packages("datawizard")
+library(datawizard)
+
+
+#extra packages for extra functionality
+install.packages("readr")
+library(readr)
+install.packages("amelia")
+library(amelia)
+install.packages("tidyverse")
+library(tidyverse)
+
+#print an optimised amount of the results from the function
+option(maxprint=1000000)
