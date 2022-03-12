6641b45f927bab22df84810705759f9d73d7463d
simisani ndaba <53818579+sndaba@users.noreply.github.com>
2022-03-12 22:49:32 +0200

    Add files via upload

---
+++ Using_R_as_an_Epidemiologist/power_calculation.R
@@ -0,0 +1,69 @@
+#power analysis
+library(pwr)
+library(epiR)
+
+#https://cran.r-project.org/web/packages/pwr/vignettes/pwr-vignette.html
+
+                          #effect size
+#Test	                    small	medium	large
+#chi-square tests (chisq)	  0.1	0.3	0.5
+
+#Chi square
+pwr.chisq.test(w =0.21, N = , df = 1, sig.level =0.05, power = 0.9)
+pwr.chisq.test(w =0.1, N = , df = 1, sig.level =0.05, power = 0.8)
+pwr.chisq.test(w =0.2, N = , df = 1, sig.level =0.05, power = 0.9)
+pwr.chisq.test(w =0.2, N = , df = 1, sig.level =0.05, power = 0.8)
+pwr.chisq.test(w =0.3, N = , df = 1, sig.level =0.05, power = 0.8)
+pwr.chisq.test(w =0.3, N = , df = 1, sig.level =0.05, power = 0.9)
+pwr.chisq.test(w =0.5, N = , df = 1, sig.level =0.05, power = 0.8)
+pwr.chisq.test(w = , N = 225, df = 1, sig.level =0.05, power = 0.9)
+pwr.chisq.test(w = , N = 297, df = 1, sig.level =0.05, power = 0.8)
+pwr.chisq.test(w = , N = 276, df = 1, sig.level =0.05, power = 0.8)
+
+
+#T-Test
+#Small
+pwr.t.test(d=0.2, sig.level=0.05, power=0.80, type="two.sample", alternative="two.sided")
+
+#Medium
+pwr.t.test(d=0.5, sig.level=0.05, power=0.80, type="two.sample", alternative="two.sided")
+
+#Large
+pwr.t.test(d=0.8, sig.level=0.05, power=0.80, type="two.sample", alternative="two.sided")
+
+#Small
+pwr.t.test(d=0.2, sig.level=0.05, power=0.90, type="two.sample", alternative="two.sided")
+
+#Medium
+pwr.t.test(d=0.5, sig.level=0.05, power=0.90, type="two.sample", alternative="two.sided")
+
+#Large
+pwr.t.test(d=0.8, sig.level=0.05, power=0.90, type="two.sample", alternative="two.sided")
+
+#Back-calculation
+pwr.t.test(n=130, d=, sig.level=0.05, power=0.90, type="two.sample", alternative="two.sided")
+
+pwr.t.test(n=130, d=, sig.level=0.05, power=0.80, type="two.sample", alternative="two.sided")
+
+
+power.prop.test(p1=.30, p2=.391304, power=.95) 
+#these values for p1 and p2 give OR of 1.50
+
+pwr.2p.test(h = ES.h(p1=.50, p2= 0.05), n = 225, sig.level =0.05, power = )
+pwr.2p.test(h = ES.h(p1=.50, p2= 0.10), n = 225, sig.level =0.05, power = )
+pwr.2p.test(h = ES.h(p1=.50, p2= 0.10), n = 225, sig.level =0.05, power = )
+pwr.2p.test(h = ES.h(p1=.50, p2= 0.10), n = 225, sig.level =0.05, power = )
+pwr.2p.test(h = ES.h(p1=.50, p2= 0.10), n = 225, sig.level =0.05, power = )
+pwr.2p.test(h = ES.h(p1=.50, p2= 0.10), n = 225, sig.level =0.05, power = )
+
+power.prop.test(p1=.50, p2=.25, power=.80) 
+pwr.2p.test(h = ES.h(p1=0.10, p2= 0.25), n = , sig.level =0.05, power = 0.8)
+
+pwr.2p.test(h = ES.h(p1=0.1819, p2= 0.10), n = , sig.level =0.05, power = 0.8)
+power.prop.test(p1=.1819, p2=.10, power=.80)
+
+
+library(epiR)
+pi.sscc(OR = 2.0, p1 = NA, p0 = 0.30, n = NA, power = 0.80, 
+        r = 1, phi.coef = 0, design = 1, sided.test = 2, conf.level = 0.95, 
+        method = "unmatched", nfractional = FALSE, fleiss = FALSE)$n.total
